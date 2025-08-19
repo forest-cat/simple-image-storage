e9a50c0f07d82be2f5bc93ff212691c2dad1079f
forest-cat <github@forestcat.org>
2025-08-19 17:54:10 +0200

    add config and logging

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-from fastapi import FastAPI, UploadFile, File, Response
+import configparser
+import logging
+import os
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, UploadFile, Response
 
-app = FastAPI(title="Simple Image Storage API")
+def read_config() -> dict:
+    config = configparser.ConfigParser()
+    config_name = "dev_config.ini" if os.path.exists("dev_config.ini") else "config.ini"
+    config.read(config_name)
+    config_values = {
+        "db_filename" : config.get("Database", "filename")
+    }
+    return config_values
 
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info(f"Starting up: {app.title}")
+    read_config()
+    yield
+    logger.info(f"Shutting down: {app.title}")
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title="Simple Image Storage API",
+    lifespan=lifespan,
+    contact={
+        "name": "Forestcat",
+        "url": "https://forestcat.org",
+        "email": "sis-api-contact@forestcat.org",
+    },
+    openapi_url=None, # Disable public API docs
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
